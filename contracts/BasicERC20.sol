15f1ae6253889975542736ec82413078dd21a215
EliteAmbassador <ogunbodevictor26@gmail.com>
2022-04-03 08:16:32 +0300

    Updated the basic contract

--- contracts/BasicERC20.sol
+++ contracts/BasicERC20.sol
@@ -6,9 +6,73 @@ contract BasicERC20 {
   event Transfer (address indexed from, address indexed to, uint256 value);
   event Approval (address indexed owner, address indexed spender, uint256 value);
 
+  mapping(address => uint) balances;
+  mapping(address => mapping(address => uint)) allowed;
+
+  uint public decimals;
+  uint public totalSupply_;
+
+  string public name; //CIU Christian Fellowship
+  string public symbol; //CIUCF
+
+  modifier sufficientBalance(address _spender, uint _value){
+    require(_value <= balances[_spender], "You have insufficient tokens");
+    _;
+  }
+
+  modifier sufficientApproval(address _owner, address _spender, uint _value){
+    require(_value <= allowed[_owner][_spender], "You have insufficient allowance from owner");
+    _;
+  }
+
+  modifier validAddress(address _address) {
+    require(_address != address(0), "Address is invalid, Please, check your address and try again");
+    _;
+  }
 
 
-  constructor(uint _totalSupply, uint _decimals, string) {
+  constructor(uint _totalSupply, uint _decimals, string memory _name, string memory _symbol) {
     totalSupply_ = _totalSupply;
+    decimals = _decimals;
+    name = _name;
+    symbol = _symbol;
+
+    balances[msg.sender] = totalSupply_;
+  }
+
+  function totalSupply() public view virtual returns(uint256){
+    return totalSupply_;
+  }
+
+  function balanceOf(address _who) public view virtual returns(uint256){
+    return balances[_who];
+  }
+
+   function transfer(address to, uint256 value) public virtual sufficientBalance(msg.sender, value) validAddress(to) returns(bool){
+    balances[msg.sender] = balances[msg.sender] - value;
+    balances[to] = balances[to] + value;
+    emit Transfer(msg.sender, to, value);
+
+    return true;
+  }
+
+  function approve(address spender, uint256 value) public virtual validAddress(spender) returns(bool){
+    allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+
+    return true;
+  }
+
+  function allowance(address owner, address spender) public virtual view returns(uint256){
+    return allowed[owner][spender];
+  }
+
+  function transferFrom(address from, address to, uint256 value) public virtual sufficientBalance(from, value) sufficientApproval(from, msg.sender, value) validAddress(to) returns(bool){
+    allowed[from][msg.sender] = allowed[from][msg.sender] - value; //Reduces the sender's allowance
+    balances[from] = balances[from] - value; //Reduces the allocators balance
+    balances[to] = balances[to] + value; //Increases the receiver's balance
+    emit Transfer(from, to, value);
+
+    return true;
   }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,13 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+require("dotenv").config();
+
+MNEMONIC = process.env.mnemonic;
+INFURA_PROJECT_URL = process.env.INFURA_PROJECT_URL;
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -41,11 +48,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+    host: "127.0.0.1",     // Localhost (default: none)
+    port: 8545,            // Standard Ethereum port (default: none)
+    network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -57,14 +64,15 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+    provider: () => new HDWalletProvider(MNEMONIC, INFURA_PROJECT_URL),
+    network_id: 4,       // Rinkeby's id
+    gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    networkCheckTimeout: 999999,
+    timeoutBlocks: 20000,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
